0ff52d8c9c6bfd54aca3ec46095d00013917d727
GMKmani <91533361+GMKmani@users.noreply.github.com>
2023-08-21 20:49:32 +0530

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static("...../dist"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin:["http:localhost:3001",
-                      "https://melodic-sable-45f696.netlify.app",], credentials: true }));
+                      "https://timely-cobbler-5b8055.netlify.app",], credentials: true }));
 var id1="";
 mongoose
   .connect(
