ebaa2e9a58c157919e8056b630ba35f787c725df
GMKmani <91533361+GMKmani@users.noreply.github.com>
2023-08-21 18:10:07 +0530

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", express.static("...../dist"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({ origin:["http:localhost:3001",
-                      "https://euphonious-dolphin-0a7f60.netlify.app",], credentials: true }));
+                      "https://melodic-sable-45f696.netlify.app",], credentials: true }));
 var id1="";
 mongoose
   .connect(
